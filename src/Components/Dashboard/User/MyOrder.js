9d77f1cfb8f863b0987b75ebf323c055ed82579a
proloypaul <proloyp23@gmail.com>
2021-11-11 14:22:18 +0600

    user order showing ui

--- src/Components/Dashboard/User/MyOrder.js
+++ src/Components/Dashboard/User/MyOrder.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../../Context/useAuth';
 
 const MyOrder = () => {
+    const {user} = useAuth()
+    const [userOrder, setUserOrder] = useState([])
+
+    useEffect(()=> {
+        const url = `http://localhost:3800/orders/${user.email}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            setUserOrder(data)
+        }) 
+    }, [user.email])
     return (
         <div>
             <h1>My order section</h1>
+            <h1>Your Total Order {userOrder.length}</h1>
         </div>
     );
 };
