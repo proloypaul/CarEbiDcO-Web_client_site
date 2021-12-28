355e6bf0df4cc95b4fa7304ba3c987542d9c5ec7
proloypaul <proloyp23@gmail.com>
2021-12-28 23:51:16 +0600

    payment system comming soon

--- src/Components/Dashboard/User/Pay.js
+++ src/Components/Dashboard/User/Pay.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router';
 
 const Pay = () => {
     const {paymentId} = useParams()
     // const paymentId = useParams()
     // console.log(paymentId)
-    const [payOrder, setPayOrder] = useState([])
+    // const [payOrder, setPayOrder] = useState([])
 
-    useEffect(() => {
-        const url = `http://localhost:3800/orders/${paymentId}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setPayOrder(data)
-            })
-    }, [paymentId])
+    // useEffect(() => {
+    //     const url = `http://localhost:3800/orders/${paymentId}`;
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             console.log(data)
+    //             setPayOrder(data)
+    //         })
+    // }, [paymentId])
 
 
     return (
